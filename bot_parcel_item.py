5ece037e3418ca43ab3d1b489e15d7f41578c3b9
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2020-06-07 00:28:22 +0300

    updates

--- bot_parcel_item.py
+++ bot_parcel_item.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import telebot
-import config
+import config  
 import re
+import os
 
 bot = telebot.TeleBot(config.token)
 
@@ -21,39 +22,35 @@ class parsel:
         self.result = ''
 
     def load_page(self):
-        url = 'https://1track.ru/tracking/' + '{}'.format(self.item_num)  
-        chromedriver = '/bin/chromedriver'  
+        url = 'https://1track.ru/tracking/' + '{}'.format(self.item_num)
+        path = os.path.abspath('bot_parcel_item.py')
+        base_dir = os.path.dirname(path)
+        path_chromedriver = os.path.join(base_dir, 'chromedriver')
         options = webdriver.ChromeOptions()
         options.add_argument('headless')  
-        browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+        browser = webdriver.Chrome(executable_path=path_chromedriver, options=options)
         browser.get(url)  
         requiredHtml = browser.page_source 
         return requiredHtml
 
-    def exploring_page(self, page):  # exploring html page
+    def exploring_page(self, page):  
         soup = BeautifulSoup(page, 'lxml')
         row_parsel = soup.select('div.show_nogroups')
         
-        # searchng points of arrivel
         for point in row_parsel:  
             points_of_arrivel = point.find_all('div', attrs={'class': 'stage'})
             
-            # getting info about points of arrivel
             for block in points_of_arrivel:  
                 title = block.find('div', attrs={'class': 'col-12 col-md-8 statuses-block'})
-                # sorting advertisement
                 if title == None:  
                     pass
                 else:
-                    # getting str about transfer
                     info_about_po = title.h4['data-lang-ru']  
                     getting_time = block.find('div', attrs={'class': 'col-12 col-md-2 stage-timing stage-transit'})
                     time = getting_time.select_one('p.date').text
-                    # conversion to a single form
                     self.result = self.result + time + ' ' + info_about_po + '\n'  
         return self.result
-    
-    # manage func
+
     def run(self):  
         page = self.load_page()
         finish = self.exploring_page(page=page)
